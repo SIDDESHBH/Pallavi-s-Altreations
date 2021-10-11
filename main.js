0274f47df08e8223521ffc5f3e28bdbb74cc7a0c
SIDDESHBH <91704862+SIDDESHBH@users.noreply.github.com>
2021-10-11 15:41:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function calculate()
+{
+    document.getElementById("Area").innerHTML=Number(document.getElementById("length").value)*
+    Number(document.getElementById("length").value);
+    document.getElementById("perimeter").innerHTML=Number(document.getElementById("length").value)*4;
+     
+}
